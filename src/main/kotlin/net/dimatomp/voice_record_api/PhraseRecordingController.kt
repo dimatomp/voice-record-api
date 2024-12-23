41a439a2658dd7df9e3f4377620901cd7de0f2ea
dimatomp <dmitrytomp@gmail.com>
2024-12-23 19:50:08 +0900

    aac extraction to a file works

--- src/main/kotlin/net/dimatomp/voice_record_api/PhraseRecordingController.kt
+++ src/main/kotlin/net/dimatomp/voice_record_api/PhraseRecordingController.kt
@@ -1,7 +1,11 @@
 package net.dimatomp.voice_record_api
 
+import org.jcodec.common.io.ByteBufferSeekableByteChannel
+import org.jcodec.common.model.Packet
+import org.jcodec.containers.mp4.demuxer.MP4Demuxer
 import org.mp4parser.muxer.InMemRandomAccessSourceImpl
 import org.mp4parser.muxer.container.mp4.MovieCreator
+import org.mp4parser.tools.ByteBufferByteChannel
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
@@ -13,7 +17,12 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.multipart.MultipartFile
 import java.io.*
+import java.nio.ByteBuffer
 import java.nio.channels.Channels
+import java.nio.channels.FileChannel
+import java.nio.channels.SeekableByteChannel
+import java.nio.file.Path
+import java.nio.file.StandardOpenOption
 import javax.sound.sampled.*
 
 
@@ -27,46 +36,53 @@ class PhraseRecordingController @Autowired constructor(private val executor: Thr
             // resorting to reading the entire input before processing.
             // TODO Add validation rules to limit input size according to config
             val bulkFileRead = file.resource.contentAsByteArray
-            val movie = MovieCreator.build(Channels.newChannel(ByteArrayInputStream(bulkFileRead)), InMemRandomAccessSourceImpl(bulkFileRead), file.name)
-            movie.tracks
+            val demuxer = MP4Demuxer.createMP4Demuxer(ByteBufferSeekableByteChannel(ByteBuffer.wrap(bulkFileRead), bulkFileRead.size))
+            demuxer.audioTracks
         } catch (e: Exception) {
             e.printStackTrace()
             return ResponseEntity("Invalid media format, only .m4a files are supported", HttpStatus.BAD_REQUEST)
         }
-        if (tracks.size != 1 || tracks[0].handler != "soun")
-            return ResponseEntity("Only regular audio files are supported", HttpStatus.BAD_REQUEST);
+        if (tracks.size != 1)
+            return ResponseEntity("No audio track found in MP4 file", HttpStatus.BAD_REQUEST);
         val pipeIn = PipedInputStream()
         val writeFuture = this.executor.submit {
             try {
-                BufferedInputStream(FileInputStream("input.aac")).use { fileInput ->
-                    val encoded = AudioSystem.getAudioInputStream(fileInput)
-                    val encodedFormat = encoded.format
-                    val decoded = AudioSystem.getAudioInputStream(
-                        AudioFormat(
-                            AudioFormat.Encoding.PCM_SIGNED, encodedFormat.sampleRate, 16,
-                            encodedFormat.channels, 2 * encodedFormat.channels, encodedFormat.frameRate,
-                            encodedFormat.isBigEndian, encodedFormat.properties()
-                        ), encoded
-                    )
-                    val decodedFormat = decoded.format
-                    val stored = AudioSystem.getAudioInputStream(
-                        AudioFormat(
-                            AudioFormat.Encoding.PCM_FLOAT, decodedFormat.sampleRate, 32,
-                            1, 4, decodedFormat.frameRate,
-                            decodedFormat.isBigEndian, decodedFormat.properties()
-                        ), decoded
-                    )
-                    FileOutputStream("test.wav").use { AudioSystem.write(stored, AudioFileFormat.Type.WAVE, it) }
-                }
+                val encoded = AudioSystem.getAudioInputStream(BufferedInputStream(pipeIn))
+                val encodedFormat = encoded.format
+                val decoded = AudioSystem.getAudioInputStream(
+                    AudioFormat(
+                        AudioFormat.Encoding.PCM_SIGNED, encodedFormat.sampleRate, 16,
+                        encodedFormat.channels, 2 * encodedFormat.channels, encodedFormat.frameRate,
+                        encodedFormat.isBigEndian, encodedFormat.properties()
+                    ), encoded
+                )
+                val decodedFormat = decoded.format
+                val stored = AudioSystem.getAudioInputStream(
+                    AudioFormat(
+                        AudioFormat.Encoding.PCM_FLOAT, decodedFormat.sampleRate, 32,
+                        1, 4, decodedFormat.frameRate,
+                        decodedFormat.isBigEndian, decodedFormat.properties()
+                    ), decoded
+                )
+                FileOutputStream("test.wav").use { AudioSystem.write(stored, AudioFileFormat.Type.WAVE, it) }
             } catch (e: Exception) {
                 e.printStackTrace()
             }
         }
-        val pipeOut = Channels.newChannel(PipedOutputStream(pipeIn))
-        for (sample in tracks[0].samples) {
-            sample.writeTo(pipeOut)
+        PipedOutputStream(pipeIn).use {
+            FileChannel.open(Path.of("test.aac"), StandardOpenOption.WRITE, StandardOpenOption.CREATE).use { file ->
+                val outChannel = Channels.newChannel(it)
+                val track = tracks[0]
+                var packet: Packet?
+                do {
+                    packet = track.nextFrame()
+                    packet?.data?.let {
+                        //outChannel.write(packet.data)
+                        file.write(it)
+                    }
+                } while (packet != null)
+            }
         }
-        pipeOut.close()
         writeFuture.get()
         return ResponseEntity(HttpStatus.CREATED)
     }
