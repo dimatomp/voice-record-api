c795e7c079a687fb98f8c1aa2183631760c66dd1
dimatomp <dmitrytomp@gmail.com>
2024-12-23 18:52:56 +0900

    Conversion works with file i/o streams

--- src/main/kotlin/net/dimatomp/voice_record_api/PhraseRecordingController.kt
+++ src/main/kotlin/net/dimatomp/voice_record_api/PhraseRecordingController.kt
@@ -1,23 +1,21 @@
 package net.dimatomp.voice_record_api
 
+import org.mp4parser.muxer.InMemRandomAccessSourceImpl
 import org.mp4parser.muxer.container.mp4.MovieCreator
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.multipart.MultipartFile
-import org.mp4parser.muxer.InMemRandomAccessSourceImpl
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor
 import java.io.*
 import java.nio.channels.Channels
-import java.nio.channels.Pipe
-import javax.sound.sampled.AudioFileFormat
-import javax.sound.sampled.AudioFormat
-import javax.sound.sampled.AudioSystem
+import javax.sound.sampled.*
+
 
 @Controller
 @RequestMapping("/audio/user/{userId}/phrase/{phraseId}")
@@ -39,21 +37,36 @@ class PhraseRecordingController @Autowired constructor(private val executor: Thr
             return ResponseEntity("Only regular audio files are supported", HttpStatus.BAD_REQUEST);
         val pipeIn = PipedInputStream()
         val writeFuture = this.executor.submit {
-            val encoded = AudioSystem.getAudioInputStream(File("input.aac"))
-            val encodedFormat = encoded.format
-            val decoded = AudioSystem.getAudioInputStream(AudioFormat(AudioFormat.Encoding.PCM_SIGNED, encodedFormat.sampleRate, 16,
-                encodedFormat.channels, 2 * encodedFormat.channels, encodedFormat.frameRate,
-                encodedFormat.isBigEndian, encodedFormat.properties()), encoded)
-            val decodedFormat = decoded.format
-            val stored = AudioSystem.getAudioInputStream(AudioFormat(AudioFormat.Encoding.PCM_SIGNED, decodedFormat.sampleRate, decodedFormat.sampleSizeInBits,
-                1, 2, decodedFormat.frameRate,
-                decodedFormat.isBigEndian, decodedFormat.properties()), decoded)
-            AudioSystem.write(stored, AudioFileFormat.Type.WAVE, File("test.wav"))
+            try {
+                BufferedInputStream(FileInputStream("input.aac")).use { fileInput ->
+                    val encoded = AudioSystem.getAudioInputStream(fileInput)
+                    val encodedFormat = encoded.format
+                    val decoded = AudioSystem.getAudioInputStream(
+                        AudioFormat(
+                            AudioFormat.Encoding.PCM_SIGNED, encodedFormat.sampleRate, 16,
+                            encodedFormat.channels, 2 * encodedFormat.channels, encodedFormat.frameRate,
+                            encodedFormat.isBigEndian, encodedFormat.properties()
+                        ), encoded
+                    )
+                    val decodedFormat = decoded.format
+                    val stored = AudioSystem.getAudioInputStream(
+                        AudioFormat(
+                            AudioFormat.Encoding.PCM_FLOAT, decodedFormat.sampleRate, 32,
+                            1, 4, decodedFormat.frameRate,
+                            decodedFormat.isBigEndian, decodedFormat.properties()
+                        ), decoded
+                    )
+                    FileOutputStream("test.wav").use { AudioSystem.write(stored, AudioFileFormat.Type.WAVE, it) }
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
         }
         val pipeOut = Channels.newChannel(PipedOutputStream(pipeIn))
         for (sample in tracks[0].samples) {
             sample.writeTo(pipeOut)
         }
+        pipeOut.close()
         writeFuture.get()
         return ResponseEntity(HttpStatus.CREATED)
     }
